38e3c34d28ff029bf8f020442f20dd1a72d91fe2
Glitch <>
2021-04-22 15:50:50 +0000

    lots o changes

--- README's/Details.md
+++ README's/Details.md
@@ -1,2 +1,4 @@
 ## Functions and details
 #### Currently, the language hasn't 'developed' much yet. So far, I have added comments and `<p>` tags. Another thing I should mention is that it currently prints in the *console*, so I'm planning on extending it to the web, using flask or just plain html.
+
+#### Also, it restricts some whitespaces, and allows some whitespaces. For example, inside bhtml tags, whitespace is *not* restricted, but in the plain bhtml code, whitespace might be restricted.
--- main.py
+++ main.py
@@ -1,162 +1,198 @@
 import os, sys, time, re
-from flask import Flask
+import string
 
 
 fp = input('FilePath: ')
-if '.bhtml' in fp:
-  pass
-else:
-  raise Exception("The file is not a 'bhtml' file")
-try:
-  f = open(fp,'r')
-except:
-  raise Exception("There is no such file")
 
-content = f.read()
-colist = content.split("\n")
-'''
-load = 0
-for i in colist:
-  if i:
-    load+=1
-'''
-
-os.system('clear')
-while True:
-  inp = input('> ')
-  if inp == 'run':
-    while " " in inp:
-      inp = inp.replace(" ","")
-    if inp == 'run':
-      break
-  elif inp == 'run-project':
-    while " " in inp:
-      inp = inp.replace(" ","")
-    if inp == 'run-project':
-      break
-  elif inp == 'bhtml index.bhtml':
-    while " " in inp:
-      inp = inp.replace(" ","")
-    if inp == 'bhtml index.bhtml':
-      break
-  elif inp == 'index.bhtml':
-    while " " in inp:
-      inp = inp.replace(" ","")
-    if inp == 'index.bhtml':
-      break
+def parse(file):
+  print("i started parsing!!!")
+  if '.bhtml' in file:
+    pass
   else:
-    os.system(str(inp))
+    raise Exception("The file is not a 'bhtml' file")
+  try:
+    f = open(file,'r')
+  except:
+    raise Exception("There is no such file")
+    
+  content = f.read()
+  colist = content.split("\n")
+  '''
+  load = 0
+  for i in colist:
+    if i:
+      load+=1
+  '''
 
-'''
-num=0
-while num < load:
-  print("Compiling... |")
-  time.sleep(0.08)
-  os.system('clear')
-  print("Compiling... \ ")
-  time.sleep(0.08)
-  os.system('clear')
-  print("Compiling... -")
-  time.sleep(0.08)
-  os.system('clear')
-  print("Compiling... |")
-  time.sleep(0.08)
-  os.system('clear')
-  print('Compiling... /')
-  time.sleep(0.08)
-  os.system('clear')
-  num+=1
-'''
 
-def check():
+
+  '''
+  num=0
+  while num < load:
+    print("Compiling... |")
+    time.sleep(0.08)
+    os.system('clear')
+    print("Compiling... \ ")
+    time.sleep(0.08)
+    os.system('clear')
+    print("Compiling... -")
+    time.sleep(0.08)
+    os.system('clear')
+    print("Compiling... |")
+    time.sleep(0.08)
+    os.system('clear')
+    print('Compiling... /')
+    time.sleep(0.08)
+    os.system('clear')
+    num+=1
+  '''
+
+  def check():
     df = re.findall("(?<=[AZaz])?(?!\d*=)[0-9.+-]+", lines)
     df = str(df)
 
-def wait_until(somepredicate, timeout, period=0.25, *args, **kwargs):
-  mustend = time.time() + timeout
-  while time.time() < mustend:
-    if somepredicate(*args, **kwargs): return True
-    time.sleep(period)
-  return False
+  def wait_until(somepredicate, timeout, period=0.25, *args, **kwargs):
+    mustend = time.time() + timeout
+    while time.time() < mustend:
+      if somepredicate(*args, **kwargs): return True
+      time.sleep(period)
+    return False
 
-allvars = {}
-line = 0
-read_line=0
-getChar1 = "none"
-getChar2 = "none"
-getChar3 = "none"
-var1 = "Undefined variable"
-input1 = "Undefined input"
-input2 = "Undefined input"
-input3 = "Undefined input"
+  allvars = {}
+  line = 0
+  read_line=0
+  getChar1 = "none"
+  getChar2 = "none"
+  getChar3 = "none"
+  var1 = "Undefined variable"
+  input1 = "Undefined input"
+  input2 = "Undefined input"
+  input3 = "Undefined input"
 
-def pTAG():
-  try:
-    if '</p>' in lines:
-      wrd = '<p>'
-      res = lines.partition(wrd)[2]
-      res = res.replace('</p>', '')
-      #res = res.replace(' ', '')
-      res = res.replace('{getChar1}', getChar1)
-      res = res.replace('{getChar2}', getChar2)
-      res = res.replace('{getChar3}', getChar3)
-      res = res.replace("{{input1}}", input1)
-      res = res.replace("{{input2}}", input2)
-      res = res.replace("{{input3}}", input3)
-      res = res.replace("{{var1}}", var1)
+  def docTAG():
+    try:
+      if '<!DOCTYPE bhtml>' in lines:#this way is more recommended
+        bhtmldoc = True
+      elif '<!doctype bhtml>' in lines:
+        bhtmldoc = True
+      elif '<!doctype BHTML>' in lines:
+        bhtmldoc = True
+      elif '<!DOCTYPE BHTML>' in lines:
+        bhtmldoc = True
+      else:
+        pass
+    except:
+      pass
 
-      if "{{" in res:
-        if "}}" in res:
-          start = "{{"
-          end = "}}"
-          check = res[res.find(start) + len(start):res.rfind(end)]
+  def aTAG():
+    try:
+      #lines = lines.replace(' ','') PUT DELETE SPACES FUNCTION HERE
+      if ('<a href = "' in lines):
+          wrd = '<a href = "'
+          res = lines.partition(wrd)[2]
+          split_string = res.split("\">", 1)
+          res = split_string[0]
+          print(res) # been doing too much js
+          #print(lines)
+          os.system(f"touch {res}.bhtml") # creates file
+          ee = lines.partition(f'<a href = "{res}">')
+          f = ee[2]
+          f = str(f)
+          f = f.replace('</a>','')
+          #print(f)
+         # try:
           
-          if check in allvars:
-            res = res.replace('{{','')
-            res = res.replace('}}','')
-            e = allvars[check]
-            res = res.replace(check, str(e))
-          else:
-            exit()#add error
+          
+          #except:
+           # raise Exception("ERROR")
+          print("BEFORE")
+          print(res)
+          #parse(f"{res}.bhtml")
+          print("AFTER")
+          # code here, it means it passed with href tag
+      elif ('<a id = "'):
+        pass
       
-      split_string = res.split("</p>", -1)
-      res = split_string[0]
-      print(res)
+      else:
+        pass
+    except:
+      raise Exception("ERROR")
+
+  def pTAG():
+    try:
+        if '</p>' in lines:#maybe replace </p> with </>?
+          wrd = '<p>'
+          res = lines.partition(wrd)[2]
+          res = res.replace('</p>', '')
+          #res = res.replace(' ', '')
+          res = res.replace('{getChar1}', getChar1)
+          res = res.replace('{getChar2}', getChar2)
+          res = res.replace('{getChar3}', getChar3)
+          res = res.replace("{{input1}}", input1)
+          res = res.replace("{{input2}}", input2)
+          res = res.replace("{{input3}}", input3)
+          res = res.replace("{{var1}}", var1)
+
+          if "{{" in res:
+            if "}}" in res:
+              start = "{{"
+              end = "}}"
+              check = res[res.find(start) + len(start):res.rfind(end)]
+              
+              if check in allvars:
+                res = res.replace('{{','')
+                res = res.replace('}}','')
+                e = allvars[check]
+                res = res.replace(check, str(e))
+              else:
+                exit()#add error
+
+          wait_until("</p>", 0)
+          split_string = res.split("</p>", -1)
+          res = split_string[0]
+          print(res)
+        else:
+          pass
+    except:
+      raise Exception("ERROR")
+
+  def h1TAG():
+    pass
+
+
+
+  newvar = 0
+  file = open(fp)
+  readline2 = 0
+  for lines in file.readlines():
+    if "<!--" in lines:
+      wait_until("-->", 0)
+      readline2=1
+    if readline2 == 1:
+      continue
     
-    else:
-      exit()
-  except:
-    exit()
+    line+=1
+    lines = lines.replace('\n','')
+    lines = lines.replace('\t','')
+    if lines == '': 
+      pass
+    elif "<!--" in lines:
+      wait_until("-->", 0)
+      pass
+    lines = lines.rstrip()
 
-def h1TAG():
-  pass
+    if "</p>" in lines:
+      pTAG()
+
+    if "<a href" in lines:
+      aTAG()
 
 
 
-newvar = 0
-file = open(fp)
-readline2 = 0
-for lines in file.readlines():
-  if "<!--" in lines:
-    wait_until("-->", 0)
-    readline2=1
-  if readline2 == 1:
-    continue
-  
-  line+=1
-  lines = lines.replace('\n','')
-  lines = lines.replace('\t','')
+    elif lines in string.whitespace:#i might remove this
+      pass
+    
+    elif type(lines) == str:#if the code inside index.bhtml is string, it prints, like regular html
+      print(str(lines))
 
-  if lines == '': 
-    pass
-  elif "<!--" in lines:
-    wait_until("-->", 0)
-    pass
-  lines = lines.rstrip()
-
-  if "<p>" in lines:
-    pTAG()
-
-while True:#if this runs, the program in index.bhtml successfully ran
-  inp = input('> ')
-  os.system(str(inp))
+parse(fp)
